f19300f4388f7ad3207f0e50c7454399c58d3ef9
theobori <theo1.bori@epitech.eu>
2022-03-19 18:44:20 +0100

    Primality function - Finished miller_rabin
    Secondary functions
    - random integer with a fixed size (in bits)
    - check_composite

--- src/RSA.jl
+++ src/RSA.jl
@@ -2,6 +2,8 @@
 
 module RSA
 
+using Random
+
 """
 Returns x^n mod p
 """
@@ -54,8 +56,8 @@ end
 
 """
 Euler totient function of n
-phi(n) = number of positive integers co-prime with n.
 
+phi(n) = number of positive integers co-prime with n.
 n: Int
 """
 function phi(n::Integer)
@@ -75,6 +77,32 @@ function phi(n::Integer)
         result -= result / n
     end
     Int(result)
+end
+
+"""
+Returns a random Integer of size bit_size bits
+"""
+function get_random_int(bit_size::T) where T <: Integer
+    bit_vector = bitrand(bit_size)
+    sum([(2 ^ (i - 1)) * bit for (i, bit) in enumerate(reverse(bit_vector))])
+end  
+
+"""
+Estimate if the number is prime
+"""
+function check_composite(a::T, d::T, n::T, s::T) where T <: Integer
+    x = exp(a, d, n)
+
+    if (x == 1 || x == (n - 1))
+        return (false)
+    end
+    for _ in 1:s
+        x = (x * x) % n
+        if (x == (n - 1))
+            return (false)
+        end
+    end
+    true
 end
 
 """Miller Rabin primality test
@@ -97,6 +125,27 @@ function miller_rabin(n::T, k::T = 10) where T <: Integer
         d /= 2
     end
     @assert 2^s*d == n - 1
+    for _ in 1:k
+        a = rand(2:n - 1)
+        d = floor(Int, d)
+        if (check_composite(a, d, n, s))
+            return (false)
+        end
+    end
+    true
+end
+
+"""
+Generates a large a prime number by incremental search
+"""
+function generate_large_prime(bit_size::T, primality_test::Function = miller_rabin) where T <: Integer
+    p = get_random_int(bit_size)
+
+    if (p & 1 != 0)
+        p += 1
+    end
+
+    # To continue
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Test
 
 include("../src/RSA.jl")
 
-@testset "Secondary function" begin
+@testset "Secondary functions" begin
 
     @test RSA.exp(2, 47) == 199807
     @test RSA.exp(3, 51, 678) == 93
@@ -17,4 +17,14 @@ include("../src/RSA.jl")
     @test RSA.phi(10) == 4
     @test RSA.phi(7) == 6
     @test RSA.phi(33500000) == 13200000
+
+    @test RSA.get_random_int(3) <= 7
+    @test RSA.get_random_int(8) <= 255
+end
+
+@testset "Primality functions" begin
+
+    @test RSA.miller_rabin(561) == false
+    @test RSA.miller_rabin(29) == true
+    @test RSA.miller_rabin(221) == false
 end
